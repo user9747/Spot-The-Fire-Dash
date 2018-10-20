1aec0984c07d89b2da1179873174c33e83dc70b6
Bilal <bilaljaleel123@gmail.com>
2018-10-20 16:56:24 +0530

    Added store

--- src/store.js
+++ src/store.js
@@ -5,10 +5,16 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    url:null,
+    type:"Country",
   },
   mutations: {
-
+    changeType(state,newType){
+      state.type = newType;
+    },
+    updateUrl(state,newUrl){
+      state.url = newUrl
+    }
   },
   actions: {
 
