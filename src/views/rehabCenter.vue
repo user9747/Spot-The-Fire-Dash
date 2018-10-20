40557ede2f95841b1e8055e9d8c3b0bfaf46a22f
albin <albinpkr96@gmail.com>
2018-10-21 00:27:29 +0530

    vatt pidikanu

--- src/views/rehabCenter.vue
+++ src/views/rehabCenter.vue
@@ -1,46 +1,142 @@
 <template>
-  <div >
-  <form novalidate class="md-layout md-size-50 md-small-size-100" >
-<md-card class="md-layout-item md-size-50 md-small-size-100">
-  <md-card-header>
-    <div class="md-title">Add REhab Center</div>
-  </md-card-header>
-    <md-field>
-      <label>Description</label>
-      <md-input v-model="description"></md-input>
-      <span class="md-helper-text">Description</span>
-    </md-field>
+  <div>
+    <form novalidate class="md-layout md-alignment-center" @submit.prevent="validateUser">
+      <md-card class="md-layout-item md-size-50 md-small-size-100">
+        <md-card-header>
+          <div class="md-title">Add Rehab</div>
+        </md-card-header>
 
-    <md-field>
-      <label>Latitude</label>
-      <md-input v-model="lat" placeholder="A nice placeholder"></md-input>
-    </md-field>
+        <md-card-content>
+          <div class="md-layout md-gutter">
+            <div class="md-layout-item md-small-size-100">
+              <md-field :class="getValidationClass('firstName')">
+                <label for="description">Description</label>
+                <md-input name="description" id="description"  v-model="form.description" :disabled="sending" />
+              </md-field>
+            </div>
+          </div>
+          <div class="md-layout md-gutter">
+            <div class="md-layout-item md-small-size-100">
+              <md-field :class="getValidationClass('firstName')">
+                <label for="latitude">Latitude</label>
+                <md-input name="latitude" id="latitude"  v-model="form.latitude" :disabled="sending" />
+              </md-field>
+            </div>
+          </div>
+          <div class="md-layout md-gutter">
+            <div class="md-layout-item md-small-size-100">
+              <md-field :class="getValidationClass('firstName')">
+                <label for="longitude">Longitude</label>
+                <md-input name="longitude" id="longitude" autocomplete="longitude" v-model="form.longitude" :disabled="sending" />
+              </md-field>
+            </div>
+          </div>
+        </md-card-content>
 
-    <md-field md-inline>
-      <label>Longitude</label>
-      <md-input v-model="long"></md-input>
-    </md-field>
-    </md-card-content>
+        <md-progress-bar md-mode="indeterminate" v-if="sending" />
 
-        
+        <md-card-actions>
+          <md-button type="submit" class="md-primary" :disabled="sending">Create Rehab Center</md-button>
+        </md-card-actions>
       </md-card>
 
+      <md-snackbar :md-active.sync="userSaved">The user {{ lastUser }} was saved with success!</md-snackbar>
     </form>
   </div>
 </template>
 
 <script>
+  import { validationMixin } from 'vuelidate'
+  import {
+    required,
+    email,
+    minLength,
+    maxLength
+  } from 'vuelidate/lib/validators'
+
   export default {
-    name: 'TextFields',
+    name: 'FormValidation',
+    mixins: [validationMixin],
     data: () => ({
-      initial: 'Initial Value',
-      type: null,
-      withLabel: null,
-      inline: null,
-      number: null,
-      textarea: null,
-      autogrow: null,
-      disabled: null
-    })
+      form: {
+        firstName: null,
+        lastName: null,
+        gender: null,
+        age: null,
+        email: null,
+      },
+      userSaved: false,
+      sending: false,
+      lastUser: null
+    }),
+    validations: {
+      form: {
+        firstName: {
+          required,
+          minLength: minLength(3)
+        },
+        lastName: {
+          required,
+          minLength: minLength(3)
+        },
+        age: {
+          required,
+          maxLength: maxLength(3)
+        },
+        gender: {
+          required
+        },
+        email: {
+          required,
+          email
+        }
+      }
+    },
+    methods: {
+      getValidationClass (fieldName) {
+        const field = this.$v.form[fieldName]
+
+        if (field) {
+          return {
+            'md-invalid': field.$invalid && field.$dirty
+          }
+        }
+      },
+      clearForm () {
+        this.$v.$reset()
+        this.form.firstName = null
+        this.form.lastName = null
+        this.form.age = null
+        this.form.gender = null
+        this.form.email = null
+      },
+      saveUser () {
+        this.sending = true
+
+        // Instead of this timeout, here you can call your API
+        window.setTimeout(() => {
+          this.lastUser = `${this.form.firstName} ${this.form.lastName}`
+          this.userSaved = true
+          this.sending = false
+          this.clearForm()
+        }, 1500)
+      },
+      validateUser () {
+        this.$v.$touch()
+
+        if (!this.$v.$invalid) {
+          this.saveUser()
+        }
+      }
+    }
   }
 </script>
+
+<style lang="css" scoped>
+  .md-progress-bar {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+  }
+</style>
