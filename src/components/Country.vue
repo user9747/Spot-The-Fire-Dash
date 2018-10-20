a853bd91e56bc23a492513be3ecb7563091e5c60
albin <albinpkr96@gmail.com>
2018-10-20 16:56:17 +0530

    vue macha

---
+++ src/components/Country.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <ol>
+    <li v-for="key in keys">
+      {{ key }}
+      <button @click="states(key)" >hello</button>
+    </li>
+  </ol>
+  </div>
+</template>
+
+<script>
+  import firebase from 'firebase'
+  var keys=[],values=[],fires,state;
+export default {
+  name: 'Country',
+  props: {
+    msg: String
+  },  
+  data:function(){
+    return{
+      keys,
+      values,fires,state
+    }
+  },
+  methods:{
+  },
+  mounted(){
+    var fireRef = firebase.database().ref("fire_loc");
+    fireRef.on('value',function(snap){
+      fires=snap.val();
+      for(var i in fires){
+        keys.push(i);
+      }
+    });
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Country/>
+    <State/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Country from '@/components/Country.vue'
+import States from '@/components/State.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Country
   }
 }
 </script>
